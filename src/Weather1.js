ab4739912a55e22fe84791bda62efd2896b1d65a
amitabh09 <amitabhshahi09@gmail.com>
2023-02-10 19:27:14 +0530

    added wind speed and humidity

--- src/Weather1.js
+++ src/Weather1.js
@@ -4,6 +4,7 @@ import x from './Apikey';
 
 export default function Weather1() {
     const[city,setCity]=useState(null)
+    const[air,setAir]=useState(null)
     const[search,setSearch]=useState('Lucknow')
     
     useEffect(()=>{
@@ -13,6 +14,7 @@ export default function Weather1() {
             const resJson= await response.json();
             //console.log(resJson);
             setCity(resJson.main);
+            setAir(resJson.wind);
         };
         fetchApi();
     },[search])
@@ -28,11 +30,13 @@ export default function Weather1() {
         value={search}/>
         <br/>
       </div>
-      { !city ? (<div className="container-sm cnt3">No Data Found <br /> Enter First Letter Of City Name In Upper Case</div>):(
+      { !city ? (<div className="container-sm cnt3">No Data Found <br /> ! Make Sure That Spelling is Coorect</div>):(
         <div className="container-sm cnt2">
             <br/>
             <h2 className='cnt12'>{search}</h2>
-            <h3 className='cnt12'>{city.temp}</h3>
+            <h3 className='cnt12'>Temperature : {city.temp} Degrees</h3>
+            <h3 className='cnt12'>Humidity : {city.humidity} % </h3>
+            <h3 className='cnt12'>Wind Speed : {air.speed} km/hr </h3>
             <br/>
         </div>
       )}
